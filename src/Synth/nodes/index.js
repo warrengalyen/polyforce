88c80e1709e21fb38f636dcd2e15dcada0c98452
warrengalyen <wgalyen@hotmail.com>
2024-01-08 18:24:08 -0800

    Finish creating components, synth setup complete

---
+++ src/Synth/nodes/index.js
@@ -0,0 +1,9 @@
+import LFO from "./LFO";
+import VCO from "./VCO";
+import FILTER from "./Filter";
+import MODULATION from "./mod";
+import FX from "./FX";
+import DISTORTION from "./Distortion";
+import ENVELOPE from "./Envelope";
+
+export { LFO, VCO, FILTER, MODULATION, FX, DISTORTION, ENVELOPE };
